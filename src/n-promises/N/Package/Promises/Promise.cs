b87103543f272badd311dc1b62287aee758c0e2a
Doug <douglas.linder+git@gmail.com>
2021-08-19 16:42:18 +0800

    uncaught errors are now logged using Debug.Error

--- src/n-promises/N/Package/Promises/Promise.cs
+++ src/n-promises/N/Package/Promises/Promise.cs
@@ -7,196 +7,204 @@ using UnityEngine;
 
 namespace N.Package.Promises
 {
-  public class Promise : Promise<bool>
-  {
-    private readonly TaskCompletionSource<bool> _deferred;
-
-    private readonly Task _task;
-
-    public Promise(Task task) : base(null)
+    public class Promise : Promise<bool>
     {
-      _task = task;
-      _deferred = new TaskCompletionSource<bool>();
-      Task = _deferred.Task;
-    }
+        private readonly TaskCompletionSource<bool> _deferred;
 
-    public Promise Then(Action onSuccess, Action<Exception> onError = null)
-    {
-      base.Then((_) => onSuccess(), onError);
-      return this;
-    }
+        private readonly Task _task;
 
-    public override bool Check()
-    {
-      if (State != PromiseState.Pending) return true;
-
-      if (_task == null)
-      {
-        Failed(new Exception("Task was NULL for Promise"));
-        return true;
-      }
-
-      if (!_task.IsCompleted) return false;
-
-      if (_task.IsFaulted)
-      {
-        Failed(_task.Exception);
-        return true;
-      }
-
-      if (_task.IsCanceled)
-      {
-        Failed(new TaskCancelledException());
-      }
-
-      Success();
-      return true;
-    }
-
-    protected override void Success()
-    {
-      _deferred.SetResult(true);
-      base.Success();
-    }
-
-    protected override void Failed(Exception exception)
-    {
-      _deferred.SetException(exception);
-      base.Failed(exception);
-    }
-
-    /// <summary>
-    /// Run an async action and automatically dispatch it.
-    /// Use it like this:
-    ///
-    ///   Promise.Run(async () => {
-    ///     try {
-    ///       await whatever()
-    ///     }
-    ///     catch(Exception err) {
-    ///       ...
-    ///     }
-    ///   });
-    ///
-    /// This allows a more ergonomic code path than:
-    ///
-    ///   whatever().Promise().Then(() => { ... }, (err) => { .. });
-    ///  
-    /// </summary>
-    public static void Run(Func<Task> action)
-    {
-       action().Dispatch();
-    }
-  }
-
-  public class Promise<TResult> : IPromise
-  {
-    protected PromiseState State = PromiseState.Pending;
-
-    protected Task<TResult> Task;
-
-    private readonly List<Action<TResult>> _success = new List<Action<TResult>>();
-
-    private readonly List<Action<Exception>> _errors = new List<Action<Exception>>();
-
-    public Promise(Task<TResult> task)
-    {
-      Task = task;
-    }
-
-    public Promise<TResult> Then(Action<TResult> onSuccess, Action<Exception> onError = null)
-    {
-      if (State != PromiseState.Pending) return this;
-
-      if (onSuccess != null)
-      {
-        _success.Add(onSuccess);
-      }
-
-      if (onError != null)
-      {
-        _errors.Add(onError);
-      }
-
-      return this;
-    }
-
-    public virtual bool Check()
-    {
-      if (State != PromiseState.Pending) return true;
-
-      if (Task == null)
-      {
-        Failed(new Exception("Task was NULL for Promise"));
-        return true;
-      }
-
-      if (!Task.IsCompleted) return false;
-
-      if (Task.IsFaulted)
-      {
-        Failed();
-        return true;
-      }
-
-      if (Task.IsCanceled)
-      {
-        Failed(new TaskCancelledException());
-      }
-
-      Success();
-      return true;
-    }
-
-    [Obsolete]
-    public Promise<TResult> ResolveWhenDone()
-    {
-      var worker = PromiseWorker.Get();
-      worker.Register(this);
-      return this;
-    }
-
-    public Promise<TResult> Dispatch()
-    {
-      var worker = PromiseWorker.Get();
-      worker.Register(this);
-      return this;
-    }
-
-    protected virtual void Failed(Exception exception = null)
-    {
-      State = PromiseState.Rejected;
-      _errors.ForEach(i => Safe(i, exception ?? Task.Exception));
-      Clear();
-    }
-
-    protected virtual void Success()
-    {
-      State = PromiseState.Resolved;
-      _success.ForEach(i => Safe(i, Task.Result));
-      Clear();
-    }
-
-    private void Safe<T>(Action<T> action, T value)
-    {
-      try
-      {
-        action?.Invoke(value);
-      }
-      catch (Exception error)
-      {
-        // Pass tests, but still log in play mode in the editor.
-        var runningTests = Application.isEditor && !Application.isPlaying;
-        if (!runningTests) {
-          Debug.LogException(error);
+        public Promise(Task task) : base(null)
+        {
+            _task = task;
+            _deferred = new TaskCompletionSource<bool>();
+            Task = _deferred.Task;
         }
-      }
+
+        public Promise Then(Action onSuccess, Action<Exception> onError = null)
+        {
+            base.Then((_) => onSuccess(), onError);
+            return this;
+        }
+
+        public override bool Check()
+        {
+            if (State != PromiseState.Pending) return true;
+
+            if (_task == null)
+            {
+                Failed(new Exception("Task was NULL for Promise"));
+                return true;
+            }
+
+            if (!_task.IsCompleted) return false;
+
+            if (_task.IsFaulted)
+            {
+                Failed(_task.Exception);
+                return true;
+            }
+
+            if (_task.IsCanceled)
+            {
+                Failed(new TaskCancelledException());
+            }
+
+            Success();
+            return true;
+        }
+
+        protected override void Success()
+        {
+            _deferred.SetResult(true);
+            base.Success();
+        }
+
+        protected override void Failed(Exception exception)
+        {
+            _deferred.SetException(exception);
+            base.Failed(exception);
+        }
+
+        /// <summary>
+        /// Run an async action and automatically dispatch it.
+        /// Use it like this:
+        ///
+        ///   Promise.Run(async () => {
+        ///     try {
+        ///       await whatever()
+        ///     }
+        ///     catch(Exception err) {
+        ///       ...
+        ///     }
+        ///   });
+        ///
+        /// This allows a more ergonomic code path than:
+        ///
+        ///   whatever().Promise().Then(() => { ... }, (err) => { .. });
+        ///  
+        /// </summary>
+        public static void Run(Func<Task> action)
+        {
+            action().Dispatch();
+        }
     }
 
-    private void Clear()
+    public class Promise<TResult> : IPromise
     {
-      _success.Clear();
-      _errors.Clear();
+        protected PromiseState State = PromiseState.Pending;
+
+        protected Task<TResult> Task;
+
+        private readonly List<Action<TResult>> _success = new List<Action<TResult>>();
+
+        private readonly List<Action<Exception>> _errors = new List<Action<Exception>>();
+
+        public Promise(Task<TResult> task)
+        {
+            Task = task;
+        }
+
+        public Promise<TResult> Then(Action<TResult> onSuccess, Action<Exception> onError = null)
+        {
+            if (State != PromiseState.Pending) return this;
+
+            if (onSuccess != null)
+            {
+                _success.Add(onSuccess);
+            }
+
+            if (onError != null)
+            {
+                _errors.Add(onError);
+            }
+
+            return this;
+        }
+
+        public virtual bool Check()
+        {
+            if (State != PromiseState.Pending) return true;
+
+            if (Task == null)
+            {
+                Failed(new Exception("Task was NULL for Promise"));
+                return true;
+            }
+
+            if (!Task.IsCompleted) return false;
+
+            if (Task.IsFaulted)
+            {
+                Failed();
+                return true;
+            }
+
+            if (Task.IsCanceled)
+            {
+                Failed(new TaskCancelledException());
+            }
+
+            Success();
+            return true;
+        }
+
+        [Obsolete]
+        public Promise<TResult> ResolveWhenDone()
+        {
+            var worker = PromiseWorker.Get();
+            worker.Register(this);
+            return this;
+        }
+
+        public Promise<TResult> Dispatch()
+        {
+            var worker = PromiseWorker.Get();
+            worker.Register(this);
+            return this;
+        }
+
+        protected virtual void Failed(Exception exception = null)
+        {
+            State = PromiseState.Rejected;
+            _errors.ForEach(i => Safe(i, exception ?? Task.Exception));
+
+            // Always report errors at some level
+            if (_errors.Count == 0 && exception != null)
+            {
+                Debug.LogException(exception);
+            }
+
+            Clear();
+        }
+
+        protected virtual void Success()
+        {
+            State = PromiseState.Resolved;
+            _success.ForEach(i => Safe(i, Task.Result));
+            Clear();
+        }
+
+        private void Safe<T>(Action<T> action, T value)
+        {
+            try
+            {
+                action?.Invoke(value);
+            }
+            catch (Exception error)
+            {
+                // Pass tests, but still log in play mode in the editor.
+                var runningTests = Application.isEditor && !Application.isPlaying;
+                if (!runningTests)
+                {
+                    Debug.LogException(error);
+                }
+            }
+        }
+
+        private void Clear()
+        {
+            _success.Clear();
+            _errors.Clear();
+        }
     }
-  }
 }
--- src/n-promises/N/Package/Promises/TaskExtensions.cs
+++ src/n-promises/N/Package/Promises/TaskExtensions.cs
@@ -2,33 +2,33 @@ using System.Threading.Tasks;
 
 namespace N.Package.Promises
 {
-  public static class TaskExtensions
-  {
-    public static Promise Promise(this Task self)
+    public static class TaskExtensions
     {
-      return new Promise(self);
-    }
+        public static Promise Promise(this Task self)
+        {
+            return new Promise(self);
+        }
 
-    public static Promise<TResult> Promise<TResult>(this Task<TResult> self)
-    {
-      return new Promise<TResult>(self);
-    }
+        public static Promise<TResult> Promise<TResult>(this Task<TResult> self)
+        {
+            return new Promise<TResult>(self);
+        }
 
-    /// <summary>
-    /// Simply dispatch a task to resolve itself later, without worrying about the related promise.
-    /// </summary>
-    public static void Dispatch(this Task self)
-    {
-      self.Promise().Dispatch();
-    }
+        /// <summary>
+        /// Simply dispatch a task to resolve itself later, without worrying about the related promise.
+        /// </summary>
+        public static void Dispatch(this Task self)
+        {
+            self.Promise().Dispatch();
+        }
 
-    /// <summary>
-    /// Simply dispatch a task to resolve itself later, without worrying about the related promise.
-    /// Note that in this case the return value is simply discarded.
-    /// </summary>
-    public static void Dispatch<TResult>(this Task<TResult> self)
-    {
-      self.Promise().Dispatch();
+        /// <summary>
+        /// Simply dispatch a task to resolve itself later, without worrying about the related promise.
+        /// Note that in this case the return value is simply discarded.
+        /// </summary>
+        public static void Dispatch<TResult>(this Task<TResult> self)
+        {
+            self.Promise().Dispatch();
+        }
     }
-  }
 }
